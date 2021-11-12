7ba30f90da0b0076608c22f031723fbc770a5971
k0102575 <k0102575@gmail.com>
2021-11-12 16:16:36 +0900

    feat: naver login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import { useEffect, useRef } from 'react';
+import styled from 'styled-components';
+
+const NaverButton = styled.div`
+  display: none;
+`;
+
 function App() {
-  return <>Hello World</>;
+  const { naver } = window;
+
+  const naverRef = useRef();
+
+  const handleClick = () => {
+    naverRef.current.children[0].click();
+  };
+
+  useEffect(() => {
+    const { REACT_APP_NAVER_CLIENT_ID, REACT_APP_NAVER_CALLBACK_URL } =
+      process.env;
+
+    const naverLogin = new naver.LoginWithNaverId({
+      clientId: REACT_APP_NAVER_CLIENT_ID,
+      callbackUrl: REACT_APP_NAVER_CALLBACK_URL,
+      isPopup: false,
+      loginButton: { color: 'green', type: 3, height: 60 },
+    });
+
+    naverLogin.init();
+    naverLogin.logout();
+  }, [naver]);
+
+  return (
+    <>
+      <NaverButton id="naverIdLogin" ref={naverRef}></NaverButton>
+      <button type="button" onClick={handleClick}>
+        인증
+      </button>
+    </>
+  );
 }
 
 export default App;
